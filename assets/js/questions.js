0222e4aa77109b7579146c3c741bc436c7b7fb1f
Xandi <santosal2018@gmail.com>
2023-07-26 23:14:34 -0400

    base html / css

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+let questions_list = [
+    {
+        question : 'Inside which HTML element do you Javascript?',
+        options: ['<javascript>', '<js>', '<script>', '<ecma>'],
+        answer: '<script>'
+    },
+    {
+        question : 'Which of the following is the correct way of adding a comment in Javascript?',
+        options: ['<!--Comment-->', '#Comment', "'Comment'", '//Comment'],
+        answer: '//Comment'
+    },
+    {
+        question : 'Which of the following is NOT an assignment operator in Javascript?',
+        options: ['=', '+=', '&&', '-='],
+        answer: '&&'
+    },
+    {
+        question : 'Which of the following is the correct way to write a "for" loop in Javascript?',
+        options: ['for (i to 10)', 'for (i = 0; i < 10; i++)', 'for (i = 10; i--)', 'for (i = 7: i <= 30: i--)'],
+        answer: 'for (i = 0; i < 10; i++)'
+    },
+    {
+        question : 'How many times will this Javascript "for" loop excecute? "for (i = 1; i < 11; i++)"',
+        options: ['0', '9', '10', '11'],
+        answer: '10'
+    }
+]
