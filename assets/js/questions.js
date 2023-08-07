fc231779aa86321e2a10c35ab7b1b262c9b2336a
Xandi <santosal2018@gmail.com>
2023-08-06 21:20:31 -0400

    fix script js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 let questions_list = [
     {
         question : 'Inside which HTML element do you Javascript?',
-        options: ['<javascript>', '<js>', '<script>', '<ecma>'],
+        options: ['A = <javascript>', 'B = <js>', 'C = <script>', 'D = <ecma>'],
         answer: '<script>'
     },
     {
         question : 'Which of the following is the correct way of adding a comment in Javascript?',
-        options: ['<!--Comment-->', '#Comment', "'Comment'", '//Comment'],
+        options: ['A = <!--Comment-->', 'B = #Comment', "C = 'Comment'", 'D = //Comment'],
         answer: '//Comment'
     },
     {
         question : 'Which of the following is NOT an assignment operator in Javascript?',
-        options: ['=', '+=', '&&', '-='],
+        options: ['A = "="', 'B = "+="', 'C = "&&"', 'D = "-="'],
         answer: '&&'
     },
     {
         question : 'Which of the following is the correct way to write a "for" loop in Javascript?',
-        options: ['for (i to 10)', 'for (i = 0; i < 10; i++)', 'for (i = 10; i--)', 'for (i = 7: i <= 30: i--)'],
+        options: ['A = for (i to 10)', 'B = for (i = 0; i < 10; i++)', 'C = for (i = 10; i--)', 'D = for (i = 7: i <= 30: i--)'],
         answer: 'for (i = 0; i < 10; i++)'
     },
     {
         question : 'How many times will this Javascript "for" loop excecute? "for (i = 1; i < 11; i++)"',
-        options: ['0', '9', '10', '11'],
+        options: ['A = 0', 'B = 9', 'C = 10', 'D = 11'],
         answer: '10'
     }
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,6 @@ var questionBox = document.querySelector('.question');
 let test = 0
 let questionNumber = 0;
 
-let questions_list = [
-    {
-        question : 'Inside which HTML element do you Javascript?',
-        options: ['A = <javascript>', 'B = <js>', 'C = <script>', 'D = <ecma>'],
-        answer: '<script>'
-    },
-    {
-        question : 'Which of the following is the correct way of adding a comment in Javascript?',
-        options: ['A = <!--Comment-->', 'B = #Comment', "C = 'Comment'", 'D = //Comment'],
-        answer: '//Comment'
-    },
-    {
-        question : 'Which of the following is NOT an assignment operator in Javascript?',
-        options: ['A = "="', 'B = "+="', 'C = "&&"', 'D = "-="'],
-        answer: '&&'
-    },
-    {
-        question : 'Which of the following is the correct way to write a "for" loop in Javascript?',
-        options: ['A = for (i to 10)', 'B = for (i = 0; i < 10; i++)', 'C = for (i = 10; i--)', 'D = for (i = 7: i <= 30: i--)'],
-        answer: 'for (i = 0; i < 10; i++)'
-    },
-    {
-        question : 'How many times will this Javascript "for" loop excecute? "for (i = 1; i < 11; i++)"',
-        options: ['A = 0', 'B = 9', 'C = 10', 'D = 11'],
-        answer: '10'
-    }
-]
-
 
 function startTimer() {
     startButton.addEventListener("click", () => {
