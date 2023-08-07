aa0b52420e8600ac6b83d7baeeed56325f9a4e62
Xandi <santosal2018@gmail.com>
2023-08-06 21:30:43 -0400

    fix bug in logic

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 let questions = [
     {
         title : 'Inside which HTML element do you Javascript?',
-        choices: ['A = <javascript>', 'B = <js>', 'C = <script>', 'D = <ecma>'],
+        choices: ['<javascript>', '<js>', '<script>', '<ecma>'],
         answer: '<script>'
     },
     {
         title : 'Which of the following is the correct way of adding a comment in Javascript?',
-        choices: ['A = <!--Comment-->', 'B = #Comment', "C = 'Comment'", 'D = //Comment'],
+        choices: ['<!--Comment-->', '#Comment', "'Comment'", '//Comment'],
         answer: '//Comment'
     },
     {
         title : 'Which of the following is NOT an assignment operator in Javascript?',
-        choices: ['A = "="', 'B = "+="', 'C = "&&"', 'D = "-="'],
-        answer: '&&'
+        choices: ['=', '+=', '&&', '-='],
+        answer: '&'
     },
     {
         title : 'Which of the following is the correct way to write a "for" loop in Javascript?',
-        choices: ['A = for (i to 10)', 'B = for (i = 0; i < 10; i++)', 'C = for (i = 10; i--)', 'D = for (i = 7: i <= 30: i--)'],
+        choices: ['for (i to 10)', '(i = 0; i < 10; i++)', 'for (i = 10; i--)', 'for (i = 7: i <= 30: i--)'],
         answer: 'for (i = 0; i < 10; i++)'
     },
     {
         title : 'How many times will this Javascript "for" loop excecute? "for (i = 1; i < 11; i++)"',
-        choices: ['A = 0', 'B = 9', 'C = 10', 'D = 11'],
+        choices: ['11', '10', '9', '8'],
         answer: '10'
     }
 ]
